35332667cca4887559b965dc85333ff8b12605cf
Prabhjot <61209227+08prabhjot@users.noreply.github.com>
2022-10-29 08:29:40 +0530

    Added Stage1 code

---
+++ src/main/kotlin/stage1.kt
@@ -0,0 +1,27 @@
+//Stage- 1st
+// Objectives-
+// Print exactly the same lines as shown in the example below.
+
+ /* 
+ Example:
+ 145 centimeters is 1.45 meters
+2 miles is 3.2187 kilometers
+5.5 inches is 139.7 millimeters
+12 degrees Celsius is 53.6 degrees Fahrenheit
+3 pounds is 1.360776 kilograms 
+*/
+
+// To print this added the below code...
+
+fun stage1() {
+    println("145 centimeters is 1.45 meters\n" + 
+"2 miles is 3.2187 kilometers\n" +
+"5.5 inches is 139.7 millimeters\n" +
+"12 degrees Celsius is 53.6 degrees Fahrenheit\n" +
+"3 pounds is 1.360776 kilograms"
+)
+}
+
+fun main(){
+    stage1()
+}
