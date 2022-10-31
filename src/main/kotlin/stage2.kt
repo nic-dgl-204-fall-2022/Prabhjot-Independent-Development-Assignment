07d45e6876afe26b9a7e5d6a02d553e743075681
Prabhjot <61209227+08prabhjot@users.noreply.github.com>
2022-11-01 00:08:10 +0530

    Added stage2 code

---
+++ src/main/kotlin/stage2.kt
@@ -0,0 +1,7 @@
+//stage2 code 
+
+fun main() {
+    println("Enter a number of kilometers: ")
+    val kilometers = readLine()!!.toInt()
+    println("$kilometers kilometers is ${1000 * kilometers} meters")
+}
